c36102ae6e36f921cda977713e96a2d2dd998701
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2023-03-18 11:53:24 +0000

    added comments to grid script

--- Grid.gd
+++ Grid.gd
@@ -1,6 +1,6 @@
 class_name Grid
 extends Node2D
 
-@export var width: int = 12
-@export var height: int = 12
-@export var cell_size: int = 128
+@export var width: int = 12 #size of the grid x
+@export var height: int = 12 #size of the grid y
+@export var cell_size: int = 128 #size of each grid cell in pixels
