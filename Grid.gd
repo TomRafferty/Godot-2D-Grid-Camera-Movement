14f2831a9d34f10117b85c9be2609133d4802249
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2023-03-18 12:10:57 +0000

    added debug to show grid and cell vectors

--- Grid.gd
+++ Grid.gd
@@ -4,6 +4,7 @@ extends Node2D
 @export var width: int = 12 #size of the grid x
 @export var height: int = 12 #size of the grid y
 @export var cell_size: int = 128 #size of each grid cell in pixels
+@export var show_debug: bool = true #switch for debug(shows the grid)
 
 var grid: Dictionary = {} #stores our grid -> Vector2 keys corresponding to a grid position
 
@@ -12,6 +13,16 @@ func generate_grid():
 	for x in width:
 		for y in width:
 			grid[Vector2(x,y)] = null #null simply indicating that the space is empty
+			if show_debug:
+				var rect = ReferenceRect.new()
+				rect.position = grid_to_world(Vector2(x,y))
+				rect.size = Vector2(cell_size, cell_size)
+				rect.editor_only = false
+				add_child(rect)
+				var label = Label.new()
+				label.position = grid_to_world(Vector2(x,y))
+				label.text = str(Vector2(x,y))
+				add_child(label)
 
 func grid_to_world(pos: Vector2) -> Vector2:
 	#takes a grid position and multiplies it by
